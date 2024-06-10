0408de193c260cde210e043b65bcb4407a081837
rajdubey <raj.dubey@cometchat.com>
2024-06-10 21:57:46 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {  CometChatConversationsWithMessages, CometChatDetails, CometChatGroupsWithMessages, CometChatMessageHeader, CometChatUsersWithMessages, MessageHeaderConfiguration, MessageListConfiguration } from '@cometchat/chat-uikit-angular'; //import the component in your module.ts file
+import { CometChatConversationsWithMessages, CometChatDetails, CometChatGroupsWithMessages, CometChatIncomingCall, CometChatMessageHeader, CometChatUsersWithMessages, MessageHeaderConfiguration, MessageListConfiguration } from '@cometchat/chat-uikit-angular'; //import the component in your module.ts file
 import { CometChat } from '@cometchat/chat-sdk-javascript'
 import { SignalStateModule } from './signal-state/signal-state.module';
 
@@ -9,23 +9,19 @@ import { SignalStateModule } from './signal-state/signal-state.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CometChatUsersWithMessages, CometChatMessageHeader, CometChatDetails, SignalStateModule,CometChatConversationsWithMessages,CometChatGroupsWithMessages],
+  imports: [CometChatUsersWithMessages, CometChatMessageHeader, CometChatDetails, SignalStateModule, CometChatConversationsWithMessages, CometChatGroupsWithMessages, CometChatIncomingCall],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
-  user:CometChat.User = null as unknown as CometChat.User;
-  
+  user: CometChat.User = null as unknown as CometChat.User;
+
   constructor() {
     CometChat.getLoggedInUser().then((user: CometChat.User) => {
-      setTimeout(()=>{
+      setTimeout(() => {
         this.user = user
-      },2000)
+      }, 2000)
     })
   }
 }
-
-
-
-
