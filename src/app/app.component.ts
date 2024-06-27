50656473a494271182316abc3ecaa4c19eed061b
ajay.cometchat <ajay.gajra@cometchat.com>
2024-06-27 20:19:39 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CometChatConversationsWithMessages, CometChatDetails, CometChatGroupsWithMessages, CometChatIncomingCall, CometChatMessageHeader, CometChatUsersWithMessages, MessageHeaderConfiguration, MessageListConfiguration } from '@cometchat/chat-uikit-angular'; //import the component in your module.ts file
+import { CometChatConversationsWithMessages, CometChatDetails, CometChatGroupsWithMessages, CometChatIncomingCall, CometChatMessageHeader, CometChatUsersWithMessages, MessageHeaderConfiguration, MessageListConfiguration ,CometChatThemeService} from '@cometchat/chat-uikit-angular'; //import the component in your module.ts file
 import { CometChat } from '@cometchat/chat-sdk-javascript'
 import { SignalStateModule } from './signal-state/signal-state.module';
 
